8e609df981925dbada21ffdf763401cf3146139b
LaelV <lael.vander@gmail.com>
2022-12-13 14:42:20 +0100

    patient rdv

--- src/Form/RDVType.php
+++ src/Form/RDVType.php
@@ -4,20 +4,26 @@ namespace App\Form;
 
 use App\Entity\RDV;
 use App\Entity\Statut;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Medecin;
+use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RDVType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder            
-            ->add('statut', EntityType::class, array('class'=>Statut::class, 'choice_label' => 'libelle'))
-            ->add('save', SubmitType::class, array('label' => 'Modifier le RDV'));
+    {   
+        $builder
+            ->add('date')
+            ->add('heure')
+            ->add('medecin', EntityType::class, array(
+                'class'=>Medecin::class,
+                'choice_label'=>'nom'
+            ))
+            ->add('submit', SubmitType::class)
         ;
     }
 
